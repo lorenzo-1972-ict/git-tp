e663507c7a1de7863c7ca1b85a17d591d4f5963e
lorenzo-1972-ict <lem.ict@outlook.com>
2025-03-19 11:59:12 +0100

    message

--- app.py
+++ app.py
@@ -3,3 +3,8 @@
 import logging
 
 print("Hello Git sur la branche Feature/logging!!")
+
+text = input("Veuillez rentrer votre nom")
+
+if(len(text) < 3):
+    logging.warning('Le nom doit avoir au moins 3 char')  # will print a message to the console
