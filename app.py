df801486f4ce1e06ba73ffa2bf01647dad6d3be7
lorenzo-1972-ict <lem.ict@outlook.com>
2025-03-19 13:11:23 +0100

    update app.py for improved logging and user prompts

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# script with app code
+# script with app code to illustrate git branching
 
 import logging
 
-print("Hello Git sur la branche Feature/logging!!")
+print("Hello Git (on the feature/logging branch!!")
 
-text = input("Veuillez rentrer votre nom")
+text = input("Enter your name:")
 
 if(len(text) < 3):
-    logging.warning('Le nom doit avoir au moins 3 char')  # will print a message to the console
+    logging.warning("Your name must at least be 3 chararacters long")  # will print a message to the console
